5a21c6e5dc61d56d064d1a7817ff540018a021ad
YDT007 <ydutta2017@gmail.com>
2024-02-08 04:52:45 +0530

    Update 8051.py

--- 8051.py
+++ 8051.py
@@ -24,8 +24,9 @@ class MOV():
     def toReg(self,i,r):
         global Acc
         r.rbank[i]=Acc
-    def toRegbank_add(self,i,j,r):
-        r.rbank[i]=r.rbank[j]
+    def toAccimediate(self,r_val):
+        global Acc
+        Acc=r_val
 
 
 #INCREMENT/Decrement statement
@@ -86,16 +87,22 @@ def MOV2(i,r):
     r.toAcc(i,r)
 def MOV3(i,r):
     r.toReg(i,r)
+def MOV4(r_val):
+    global Acc
+    Acc=r_val    
 def ADD1(i,r):
     global Acc
     Acc=Acc+r.rbank[i]
+def ADD2(r_val):
+    global Acc
+    Acc=Acc+r_val
 def  INC(i,r):
     r.inc(i,r)
 def DEC(i,r):
     r.dec(i,r)       
 #Addition,Increment,Decrement,Set and Clear functions for their respective Mnemonics    
 
-Mnemonics["mov"]=MOV1
+#Mnemonics["mov"]=MOV1
 Mnemonics["add"]=ADD1
 Mnemonics["inc"]=INC
 Mnemonics["dec"]=DEC
@@ -110,17 +117,42 @@ Mnemonics["dec"]=DEC
 Acc=10        
 x=regBank()
 x1=I_D_S_C() 
+def selecting_mov(row):
+    if len(row)==3:
+       column1,column2,column3 = row
+       if column1=='mov' and column2.startswith('R') and column3.startswith('#'):
+          Mnemonics["mov"]=MOV1
+       elif column1=='mov' and  column2.startswith('A') and column3.startswith('R'):
+          Mnemonics["mov"]=MOV2     
+       elif column1=='mov' and  column2.startswith('R') and column3.startswith('A'):
+          Mnemonics["mov"]=MOV3
+       elif column1=='mov' and  column2.startswith('A') and column3.startswith('#'):       
+          Mnemonics["mov"]=MOV4
+       else
+           print('Invalid Syntax')  
+def selecting_add(row):
+    if len(row)==3:
+       column1,column2,column3 = row
+       if column1=='add' and  column2.startswith('A') and column3.startswith('R'):
+          Mnemonics["add"]=ADD1
+       elif column1=='add' and  column2.startswith('A') and column3.startswith('#'):
+          Mnemonics["add"]=ADD2
+               
 def process_row(row):
     # This function can be customized to perform operations on each row
-    column1, column2, column3 = row[0].split()
-    print(row[0].split())
-    column2_int = int(column2)
-    column3_int = int(column3)
+    if len(row)==3:
+        column1,column2,column3 = row
+    else:
+        column1,column2=row
+    print(row)
     # print(str(type(column2_int)) + " " + str(type(column3_int)))
     #(columnn1).lower()
     #print(x.rbank[column2_int][0])
     z=Mnemonics[column1]
-    if column1=='mov':
+
+    if column1=='mov' and column3.startswith('#'):
+        column3_int=int(column3[1:])
+        column2_int=int(column2[1:])
         z(x,column2_int,column3_int)
     else:
         z(column2_int,x1)
@@ -134,11 +166,12 @@ def read_csv_file(file_path):
         
         for row in reader:
             # Assuming each row has three columns
-            if len(row) <=3:
+            row.split()
+            row[1].split(',')
+            if len(row)<=3:
                 process_row(row)
-
             else:
-                print("Invalid comand")
+                print("Invalid command")
 file_path = "D:\PySim8051\PySim8051\Command.txt"  # Replace with the actual file path
 read_csv_file(file_path)
 print(x.print_register_bank())  
